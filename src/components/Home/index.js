ec906ee46f24b47d4bfdbbe4feca09184e93c35f
lt00lt00lt <494858549@qq.com>
2019-11-22 15:47:42 +0800

    '组件绑值

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state({
+            msg: "我是一个Home组件"
+        })
+    }
     render() {
         return (
             <div>
                 <h2>Hello World !</h2>
+                <h4>{this.state.msg}</h4>
             </div>
         )
     }
